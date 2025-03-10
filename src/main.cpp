d0262d887a92a4d3f0263b5172cad66241aa55dd
Stellaris57 <baskakoviv57@gmail.com>
2025-03-10 20:08:47 +0300

    make branch-new

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-#include "utils.cpp"
+
+int add(int x, int y);
+int subtract(int x, int y);
+int subtract_(int x, int y);
+int umnozh(int x, int y);
 
 int main() {
     int a = 2;
@@ -8,4 +12,6 @@ int main() {
     std::cout << "PLUS: " << add(a, b) << std::endl;
     std::cout << "MINUS: " << subtract(a, b) << std::endl;
     std::cout << "UMNOZH: " << umnozh(a, b) << std::endl;
+    std::cout << "MINUS: " << subtract_(a, b) << std::endl;
+    return 0;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,12 +1,17 @@
 
+
 int add(int x, int y) {
-    return x + y;
+    return (x + y);
 }
 
 int subtract(int x, int y) {
-    return x - y;
+    return (x - y);
 }
 
 int umnozh(int x, int y) {
-    return x * y;
+    return (x * y);
+}
+
+int subtract_(int x, int y) {
+    return (y - x);
 }
